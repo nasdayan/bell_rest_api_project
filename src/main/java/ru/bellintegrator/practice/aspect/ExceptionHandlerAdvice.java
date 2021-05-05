4a93df87991ffa4b0f665887767d77c5903df7bb
Dayan <ra1nbra1nmail@gmail.com>
2021-05-05 21:33:24 +0500

    Добавлен логгер в возможных эксепшнах

--- src/main/java/ru/bellintegrator/practice/aspect/ExceptionHandlerAdvice.java
+++ src/main/java/ru/bellintegrator/practice/aspect/ExceptionHandlerAdvice.java
@@ -1,5 +1,7 @@
 package ru.bellintegrator.practice.aspect;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,22 +15,27 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandlerAdvice.class.getName());
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseError handle(Exception exception) {
-        return new ResponseError(exception.getMessage());
+        logger.error("Код ошибки: " + HttpStatus.INTERNAL_SERVER_ERROR + ". " + exception.getMessage());
+        return new ResponseError("Код ошибки: " + HttpStatus.INTERNAL_SERVER_ERROR + ". " + exception.getMessage());
     }
 
     @ExceptionHandler(ItemNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     public ResponseError handle(ItemNotFoundException exception) {
-        return new ResponseError(exception.getMessage());
+        logger.error("Код ошибки: " + HttpStatus.NOT_FOUND + ". " + exception.getMessage());
+        return new ResponseError("Код ошибки: " + HttpStatus.NOT_FOUND + ". " + exception.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     public ResponseError handle(MethodArgumentNotValidException exception) {
-        return new ResponseError(exception.getBindingResult().getAllErrors().stream()
+        logger.error("Код ошибки: " + HttpStatus.BAD_REQUEST + ". " + exception.getMessage());
+        return new ResponseError("Код ошибки: " + HttpStatus.BAD_REQUEST + ". " + exception.getBindingResult().getAllErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining("; ")));
     }
